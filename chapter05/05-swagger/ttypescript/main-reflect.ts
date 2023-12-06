1038db86a1aea83ff929858aebc093287d98f3d3
speedphp <jake@speedphp.com>
2023-12-06 15:53:11 +0800

    修改

--- chapter05/05-swagger/ttypescript/main-reflect.ts
+++ chapter05/05-swagger/ttypescript/main-reflect.ts
@@ -1,25 +1,19 @@
 import "reflect-metadata";
-
 class MainReflect {
-    @injectAge(10)
-    private age: number;
-
-    @findReturn
+    @injectReflect
     getAge(): Number {
-        return this.age;
+        return 1;
+    }
+    @injectReflect
+    getPromise(): Promise<string> {
+        return Promise.resolve('promise');
+    }
+    @injectReflect
+    getArray(): Array<string> {
+        return ['array'];
     }
 }
-
-const mainReflect = new MainReflect();
-console.log("获得Age值：", mainReflect.getAge());
-
-function injectAge(arg: Number) {
-    return function (target: any, propertyKey: string) {
-        Object.defineProperty(target, propertyKey, {get: () => {return arg;}});
-    }
-}
-
-function findReturn(target: any, propertyKey: string) {
+function injectReflect(target: any, propertyKey: string) {
     const returnType: any = Reflect.getMetadata("design:returntype", target, propertyKey);
     console.log(target[propertyKey].name, "的返回类型是：", returnType.name);
 }
